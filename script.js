fa21f39a751d0221fef881f91f38317f58092a4c
SamyMata <130999154+SamyMata@users.noreply.github.com>
2023-05-09 09:19:03 -0300

    modificando JS

---
+++ script.js
@@ -0,0 +1,6 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    msg.innerHTML = 'Modificando Java Script'
+    img.src = 'img/fotoflores.jpg'
+}
